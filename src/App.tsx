e513ea39ac86f1ffd02d3b4c86981f86bfc410ee
jufrin01 <jufrinaha111@gmail.com>
2026-03-01 09:13:31 +0700

    continueee 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,79 @@
-import {useEffect , useState , FormEvent} from "react";
-import { User } from "./types/User";
+import {useEffect , useState } from "react";
+import type { FormEvent } from "react";
+import type {User} from "./types/User";
 import { getUser , updateUser , createUser , deleteUser} from "./services/userServices.ts";
 
 function App() {
+
+    //TODO STAMEN BUAT SET DATA
     const [user , setUser] = useState<User[]>([]);
     const [name ,setName] = useState<string>("");
     const [editId , setEditId] = useState<number |  null>(null);
 
+    //TODO FETCH DATA USER 1
     const fetchUser = async () => {
         const data = await getUser();
         setUser(data);
     };
 
+    //TODO LOAD DATA USER 2
     useEffect(() => {
-        fetchUser();
-    },[]);
+        const loadUser = async () => {
+            const data = await getUser();
+            setUser(data)
+        };
+        loadUser();
+    }, []);
 
+    //TODO logic handling submit
     const handlingSumbit = async  ( e : FormEvent)  => {
         e.preventDefault();
         if (!name) return;
-    }else {
-        await createUser(name);
-    }
+        if (editId !== null){
+            await  updateUser(editId , name);
+            setEditId(null);
+        }else {
+            await createUser(name);
+        }
 
-    setName("")
-    fetchUser();
+        setName("")
+        fetchUser();
 };
 
+    const handlingEdit = async (user: User) => {
+        setName(user.name);
+        setEditId(user.id);
+    };
 
-const
+    const handlingDelete = async (id: number) => {
+        await deleteUser(id);
+        fetchUser();
+    };
+
+    return (
+        <div>
+            <h1>CRUD REACT + AXIOS</h1>
+            <form onSubmit={handlingSumbit}>
+                <input
+                    placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <button type = "submit">
+                    {editId ? "Edit " : "Add "}
+                </button>
+            </form>
+            <hr />
+
+            {user.map((user: User) => (
+                <div key={user.id}>
+                    {user.name}
+                    <button onClick={() => handlingEdit(user)}>Edit</button>
+                    <button onClick={() => handlingDelete(user.id)}>Delete</button>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default App;
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,21 +1,25 @@
 import axios from "axios";
 import type { User } from "../types/User";
 
-const API = "http://localhost:3000";
+const API = "http://localhost:3000/users";
 
+//TODO GET DATA ENDPOINT
 export const getUser = async (): Promise<User[]> => {
     const rest = await axios.get<User[]>(API);
     return rest.data;
 };
 
+//TODO CREATE DATA ENDPOINT
 export const createUser = async (name : string) => {
     await axios.post(API, {name});
 };
 
+//TODO UPDATE DATA ENDPOINT
 export const updateUser = async (id: number, name : string) => {
-    await axios.put(`${API}/${id}`, name);
+    await axios.put(`${API}/${id}`, {name});
 };
 
+//TODO DELETE DATA ENDPOINT
 export const deleteUser = async (id: number) => {
     await axios.delete(`${API}/${id}`);
 }
